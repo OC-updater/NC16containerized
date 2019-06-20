4b5a74f00054a1bccf734303c000d3faf91caea6
OC-update <OC-update@bild.dtx.at>
2019-06-20 18:36:11 +0200

    added sleep at the end, 3rd version

--- nginx/files/entrypoint.sh
+++ nginx/files/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Load Nextcloud environment variables
-eval "$(nextcloud_env)"
+if [ -f nextcloud_env ]; then
+   eval "$(nextcloud_env)"
+fi
 
 BIN_SH="${NC_BIN_SH:=/usr/local/bin}"
 NC_ROOT="${NC_ROOTDIR:-/opt/nextcloud}"
@@ -10,6 +12,7 @@ NC_CONF="${NC_ROOT}/config"
 setup() {
    for NC_CONF in php_configure.sh acme-config.sh nextcloud.sh; do
        if [ -f "$BIN_SH/$NC_CONF" ]; then
+	   echo "----------- Install: $NC_CONF -----------"
            . $BIN_SH/$NC_CONF
        fi
    done
@@ -25,5 +28,8 @@ for SERV in php7.3-fpm nginx; do
    service $SERV start
 done
 
+#nginx -g 'daemon off;'
 
-exec /bin/bash
+#exec /bin/bash
+
+sleep 1d
